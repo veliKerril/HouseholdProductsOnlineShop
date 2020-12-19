628e38dd9d2a499a01ba6298344073c6d6c2ffd4
veliKerril <velikirilll@gmail.com>
2020-12-20 01:35:27 +0300

    v 1.4
    Исправление багов

--- prototype3/myshop3/shop/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Category, Manufacturer, Product
-
-admin.site.register(Category)
-admin.site.register(Manufacturer)
-admin.site.register(Product)
