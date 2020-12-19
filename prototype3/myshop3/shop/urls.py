1ba1cc84c10506cf8b62996833d35829a3961071
veliKerril <velikirilll@gmail.com>
2020-12-19 23:15:11 +0300

    v. 1.2
    Еще один прототип сервера с реализованной таблицой

---
+++ prototype3/myshop3/shop/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.main, name='main'),
+    url(r'^products/$', views.product_list, name='product_list'),
+    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
+
+]
