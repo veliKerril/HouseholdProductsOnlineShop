3e7d375d079e752a464d6213779a71d2bb438151
Roman Golubev <43215948+Light1984@users.noreply.github.com>
2020-12-20 21:13:51 +0300

    Update settings.py

--- prototype3/myshop3/myshop3/settings.py
+++ prototype3/myshop3/myshop3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9ubiuy)y7vq+ir4*7a#ov)!db*p#9ts2l6dwvz!9so5m#rmznj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost','188.68.221.224']
 
 
 # Application definition
