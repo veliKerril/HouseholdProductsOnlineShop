ffe608e86997f41f7dcc3938d74703f2b1e5fe68
Rin <rb1q84@gmail.com>
2020-12-24 22:36:55 +0300

    Login

--- prototype3/myshop3/myshop3/settings.py
+++ prototype3/myshop3/myshop3/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'myshop3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'allauth')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
