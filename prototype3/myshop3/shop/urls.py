17824bb142665e9d4a79646ee13f815884f7fd3e
Julia <52479551+JuliaChernykh@users.noreply.github.com>
2020-12-26 02:28:52 +0300

    1.6.2

--- prototype3/myshop3/shop/urls.py
+++ prototype3/myshop3/shop/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.main, name='main'),
     url(r'^products/$', views.product_list, name='product_list'),
     url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
+    url(r'^show_orders/$', views.show_orders, name='show_orders'),
 ]
--- prototype3/myshop3/shop/views.py
+++ prototype3/myshop3/shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from orders.models import Order, OrderItem
 from cart.forms import CartAddProductForm
 
 
@@ -49,3 +50,12 @@ def product_detail(request, id, slug):
     cart_product_form = CartAddProductForm()
     return render(request, 'shop/product/detail.html', {'product': product,
                                                         'cart_product_form': cart_product_form})
+
+
+def show_orders(request):
+    orders = Order.objects.all()
+    orders_item = OrderItem.objects.all()
+    name_of_orders = request.user.first_name
+
+    return render(request, 'shop/product/orders.html', {'orders': orders, 'orders_item': orders_item,
+                                                        'name_of_orders': name_of_orders})
