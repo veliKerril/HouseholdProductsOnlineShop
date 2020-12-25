a45b985847898e106393ff0dbdd363613f1c34f9
veliKerril <velikirilll@gmail.com>
2020-12-26 00:32:07 +0300

    v 3.3
    Fix small bags in front

--- prototype3/myshop3/orders/forms.py
+++ prototype3/myshop3/orders/forms.py
@@ -6,3 +6,13 @@ class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+
+        labels = {
+            "first_name": "",
+            "last_name": "",
+            "email": "",
+            "address": "",
+            "postal_code": "",
+            "city": "",
+        }
+
